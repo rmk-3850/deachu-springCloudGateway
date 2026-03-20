079dc2be2d0624458db1762c875a239f9c00a136
rmk-3850 <qop589@naver.com>
2026-03-20 15:49:31 +0900

    cors origin fixed

--- src/main/java/com/rm/config/SecurityConfig.java
+++ src/main/java/com/rm/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("https://dx4a7vlkmjql7.cloudfront.net","http://localhost:5173"));
+		config.setAllowedOrigins(Arrays.asList("https://deachu.site","http://localhost:5173"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(Arrays.asList("*"));
 		config.setAllowCredentials(true);
